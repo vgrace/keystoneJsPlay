2a17592f0bfe381cb8752fc2d4836bf6c49bbec8
Vivian Esquivias <v.esquivias@gmail.com>
2016-12-13 16:23:26 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+app.use('/bower_components', express.static(__dirname, '../bower_components'));
+
+module.exports = app;
