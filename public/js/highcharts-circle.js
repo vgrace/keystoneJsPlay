edf412d8bc000f20fa03467ddccab6b36c946daa
Vivian Esquivias <v.esquivias@gmail.com>
2016-12-14 16:32:09 +0100

    added shared menues and start to new dashboard

---
+++ public/js/highcharts-circle.js
@@ -0,0 +1,58 @@
+﻿$(function () {
+    Highcharts.chart('highcharts-circle-chart', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Devices',
+            colorByPoint: true,
+            data: [{
+                name: 'Livingroom',
+                y: 56.33
+            }, {
+                name: 'Kitchen',
+                y: 24.03,
+                sliced: true,
+                selected: true
+            }, {
+                name: 'Dishwasher',
+                y: 10.38
+            }, {
+                name: 'Washing machine',
+                y: 4.77
+            }, {
+                name: 'Entertainment system',
+                y: 0.91
+            }, {
+                name: 'TV',
+                y: 0.2
+            }]
+        }]
+    });
+});
---
+++ public/js/highcharts-column.js
@@ -0,0 +1,50 @@
+﻿$(function () {
+    Highcharts.chart('highcharts-basic-column-chart', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            categories: [
+
+            ],
+            crosshair: true
+        },
+        legend: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Energy Consumption (kWh)'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} kWh</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Total',
+            data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+        }]
+    });
+});
